536e0d678df22173ec3c5fe64f275f1a3dcce179
Avecoder <foravecode@gmail.com>
2022-03-28 02:55:22 +0300

    Hesus help me

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ bot.use(require('./composers/adminComposer'))
 
 expressApp.use(cors())
 
-app.use((req, res, next) => {
+expressApp.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
