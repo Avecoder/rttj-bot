d41224dd52c0eb0708722a845965d38b25a88db0
Avecoder <foravecode@gmail.com>
2022-03-31 00:26:17 +0300

    unnamed middleware

--- middlewares/unnamedMiddleware.js
+++ middlewares/unnamedMiddleware.js
@@ -1,11 +1,13 @@
 const unnamedMiddleware = async (ctx, next) => {
-	
-	if(ctx.update?.message?.text && ctx.from?.username) return next()
-	else {
-		await ctx.reply(`У тебя нет ника, ты не можешь пользоваться ботом.`)
-		return false
+
+	if(ctx.update?.message?.text) {
+		if(ctx.from?.username) return next()
+		else {
+			await ctx.reply(`У тебя нет ника, ты не можешь пользоваться ботом.`)
+			return false
+		}
 	}
-	
+
 }
 
 module.exports = unnamedMiddleware
